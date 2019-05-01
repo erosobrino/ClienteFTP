a84a7f471238bee2b987d4175636ab0fb9385ead
Ero Sobrino <ero.sobrino.dorado@gmail.com>
2019-05-01 22:43:46 +0200

    Agregar archivos de proyecto.

---
+++ ClienteFTP/ClienteFTP/ElementoLista.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ClienteFTP
+{
+    class ElementoLista
+    {
+       private Image imagen;
+        string nombre;
+        char tipo;
+        public Image Imagen { get => imagen; set => imagen = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public char Tipo { get => tipo; set => tipo = value; }
+
+        public ElementoLista()
+        {
+            Nombre = "hola";
+        }
+
+    }
+}
